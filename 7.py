e7ba7f7a2cea3b00290fa1fd093c4b782fbe4bad
jsheng2017 <32264988+jsheng2017@users.noreply.github.com>
2017-12-12 11:13:13 +0800

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+
+        # -2^31 ~ 2^31-1
+
+        if x < 0:
+            x = 0 - int(str(abs(x))[::-1])
+            if x < 0 - 2 ** 31 or x > 2 ** 31 - 1:
+                return 0
+            else:
+                return x
+        else:
+            x = int(str(x)[::-1])
+            if x < 0 - 2 ** 31 or x > 2 ** 31 - 1:
+                return 0
+            else:
+                return x
+if __name__ == '__main__':
+    ans = Solution()
+    print(ans.reverse(-123))
