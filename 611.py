5dfe2ffa3dddfd843770a80fdfa8a644ea79d5e1
jsheng2017 <32264988+jsheng2017@users.noreply.github.com>
2017-12-20 13:33:42 +0800

    Add files via upload

---
+++ 611.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def triangleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        nums = sorted(nums)
+        result = 0
+        for i in range(len(nums) - 1, 1, -1):
+            left, right = 0, i - 1
+            while left < right:
+                if nums[left] + nums[right] > nums[i] and nums[left] != 0:
+                    result += right - left
+                    right -= 1
+                else:
+                    left += 1
+
+        return result
