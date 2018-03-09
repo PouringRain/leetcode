8ac1b6450ad9148d62dccfcd71330fa424867632
jsheng2017 <32264988+jsheng2017@users.noreply.github.com>
2018-03-09 20:31:38 +0800

    "井字棋"游戏，判断当前棋盘是否合法

---
+++ 694.py
@@ -0,0 +1,37 @@
+# level: medium
+
+class Solution(object):
+    def judge(self, a, c):
+        if a[0][0] == a[0][1] == a[0][2] == c:
+            return False
+        elif a[2][0] == a[2][1] == a[2][2] == c:
+            return False
+        elif a[0][0] == a[0][1] == a[0][2] == c:
+            return False
+        elif a[0][0] == a[1][0] == a[2][0] == c:
+            return False
+        elif a[0][0] == a[1][1] == a[2][2] == c:
+            return False
+        elif a[0][2] == a[1][1] == a[2][0] == c:
+            return False
+        else:
+            return True
+
+    def validTicTacToe(self, board):
+        """
+        :type board: List[str]
+        :rtype: bool
+        """
+        nx, no = 0, 0
+        for s in board:
+            for c in s:
+                if c == 'X':
+                    nx += 1
+                elif c == 'O':
+                    no += 1
+        if nx < no or nx - no > 1:
+            return False
+        elif nx == no:
+            return self.judge(board, 'X')
+        else:
+            return self.judge(board, 'O')
