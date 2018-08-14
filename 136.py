58c69eba4a5fe9fd450fae68d08fdc85bb98a127
盛剑 <32264988+jsheng2017@users.noreply.github.com>
2018-08-14 11:35:18 +0800

    数组中只出现一次的数字

--- 136.py
+++ 136.py
@@ -1,5 +1,17 @@
 # level: eazy
 
+# best solution -->xor
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ret = 0
+        for num in nums:
+            ret ^= num
+        return ret
+
 class Solution(object):
     def singleNumber(self, nums):
         """
