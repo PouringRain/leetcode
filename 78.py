15615fe994b04eabec3ea66df881f030ddc14def
盛剑 <32264988+jsheng2017@users.noreply.github.com>
2018-08-21 19:59:09 +0800

    求数字集合的所有子集

--- 78.py
+++ 78.py
@@ -12,3 +12,26 @@ class Solution(object):
         for num in nums:
             res += [i + [num] for i in res]
         return res
+    
+ class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if not nums: return []
+        ret = []
+        curset = []
+
+        def dfs(start, curset, nums, ret):
+            # print(curset)
+            ret.append(copy.deepcopy(curset))
+            # print(res)
+            for i in range(start, len(nums)):
+                curset.append(nums[i])
+                dfs(i+1, curset, nums, ret)
+                curset.pop()
+
+        dfs(0, [], nums, ret)
+
+        return ret
