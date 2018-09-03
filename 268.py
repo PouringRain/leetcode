d98b2a55d052541432103c4cba5f08186ab71904
盛剑 <32264988+jsheng2017@users.noreply.github.com>
2018-09-03 21:24:31 +0800

    缺失数字

--- 268.py
+++ 268.py
@@ -16,3 +16,20 @@ class Solution(object):
 if __name__=='__main__':
     ans = Solution()
     print(ans.missingNumber([1,0,3]))
+    
+    
+# level: ez
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in range(len(nums)):
+            while nums[i]<len(nums) and nums[nums[i]]!=nums[i]:
+                nums[nums[i]], nums[i] = nums[i], nums[nums[i]]
+        for i in range(len(nums)):
+            if nums[i]!=i:
+                return i
+        return len(nums)
+        
